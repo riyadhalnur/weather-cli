17fae42969fba0871a774901fc17a96170cfafdb
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2015-12-03 14:35:31 +0600

    Fix linting errors

--- cli.js
+++ cli.js
@@ -42,13 +42,16 @@ weather(cli.input, function (err, result) {
 
 	if (cli.input[2] && cli.input[2] === 'C') {
 		temperature = _toCelcius(result.query.results.channel.item.condition.temp) + 'C';
-	} else if (cli.input[2] && cli.input[2] === 'F'){
+	} else if (cli.input[2] && cli.input[2] === 'F') {
 		temperature = result.query.results.channel.item.condition.temp + 'F';
 	} else {
 		temperature = _toCelcius(result.query.results.channel.item.condition.temp) + 'C';
 	}
 
-	console.log(chalk.red(cli.input[0] ? cli.input[0] : 'Dhaka' + ', ' + cli.input[1] ? cli.input[1] : 'Bangladesh'));
+	var city = cli.input[0] ? cli.input[0] : 'Dhaka';
+	var country = cli.input[1] ? cli.input[1] : 'Bangladesh';
+
+	console.log(chalk.red(city + ', ' + country));
 	console.log(chalk.cyan('Condition: ' + chalk.yellow(condition)));
 	console.log(chalk.cyan('Temperature: ' + chalk.yellow(temperature)));
 	process.exit();
