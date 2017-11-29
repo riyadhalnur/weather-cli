66de12f38c0d0021dad895070e9b823d99d433e4
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2017-11-30 00:53:23 +0600

    Refactor main weather module to use API
    Signed-off-by: Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 'use strict';
 
-const YQL = require('yql');
-const _ = require('lodash');
+const axios = require('axios');
+const API_URL = 'https://micro-weather.now.sh';
 
-module.exports = (opts, callback) => {
-	opts = opts || [];
+module.exports = (opts) => {
+  let city = opts[0] || 'Dhaka';
+  let country = opts[1] || 'Bangladesh';
 
-	let query;
-
-	if (_.isEmpty(opts)) {
-		query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="Dhaka, Bangladesh")');
-	} else {
-		query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + opts[0] + ', ' + opts[1] + '")');
-	}
-
-	query.exec((err, response) => {
-		if (err) {
-			return callback(err);
-		}
-
-		callback(null, response);
-	});
+  return axios.get(`${API_URL}?city=${city}&country=${country}`);
 };
