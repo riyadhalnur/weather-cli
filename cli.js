99f23d79ae95e5a452927b7ca5afe7f74232ac9e
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2018-02-02 16:17:53 +0800

    Update CLI file to use new API
    - Cleaned up code to be more readable. Sould only handle display and passing through commands to main handler
    - Added support for flags and better help text
    
    Signed-off-by: Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>

--- cli.js
+++ cli.js
@@ -7,52 +7,67 @@ const updateNotifier = require('update-notifier');
 const pkg = require('./package.json');
 const weather = require('./');
 
-const cli = meow({
-	help: [
-		'Usage',
-		'  $ weather <input>',
-		'',
-		'Options',
-		'  city [Default: Dhaka]',
-		'  country [Default: Bangladesh]',
-		'  scale (C/F) [Default: Celcius]',
-		'',
-		'Examples',
-		'  $ weather London UK C',
-		'  London, UK',
-		'  Condition: Partly Cloudy',
-		'  Temperature: 32C'
-	]
+const cli = meow(`
+Usage
+  $ weather <input>
+
+Options
+  --city, -ct City you want to lookup weather for
+  --country, -c Country you want to lookup weather for
+  --scale, -s Weather scale. Defaults to Celcius
+  config Set the default location
+
+Examples
+  $ weather -c Dhaka -C Bangladesh
+  Dhaka, Bangladesh
+  Condition: Partly Cloudy
+  Temperature: 32°C
+
+  $ weather config -c Dhaka -C Bangladesh
+  Default location set to Dhaka, Bangladesh
+`, {
+  flags: {
+    city: {
+      type: 'string',
+      alias: 'c'
+    },
+    country: {
+      type: 'string',
+      alias: 'C'
+    },
+    scale: {
+      type: 'string',
+      alias: 's',
+      default: 'C'
+    }
+  }
 });
 
-function _toCelcius(temp) {
-	return Math.round(((temp - 32) * 5) / 9);
+updateNotifier({ pkg }).notify();
+
+if (cli.input.length && cli.input[0] === 'config') {
+  weather.setLocation(cli.flags)
+    .then(result => {
+      console.log(chalk.bold.blue(result));
+      process.exit(0);
+    })
+    .catch(err => {
+      if (err) {
+        console.log(chalk.bold.red(err));
+        process.exit(1);
+      }
+    });
+} else {
+  weather.getWeather(cli.flags)
+    .then(result => {
+      console.log(chalk.red(`${result.city}, ${result.country}`));
+      console.log(chalk.cyan(`Condition: ${chalk.yellow(result.condition)}`));
+      console.log(chalk.cyan(`Temperature: ${chalk.yellow(result.temp)}${chalk.yellow('°' + result.scale)}`));
+      process.exit(0);
+    }).catch(err => {
+      if (err) {
+        console.log(chalk.bold.red(err));
+        process.exit(1);
+      }
+    });
 }
-
-updateNotifier({ pkg}).notify();
-
-weather(cli.input, (err, result) => {
-	if (err) {
-		console.log(chalk.bold.red(err));
-		process.exit(1);
-	}
-
-	let condition = result.query.results.channel.item.condition.text;
-	let temperature;
-
-	if (cli.input[2] && cli.input[2] === 'C') {
-		temperature = _toCelcius(result.query.results.channel.item.condition.temp) + 'C';
-	} else if (cli.input[2] && cli.input[2] === 'F') {
-		temperature = result.query.results.channel.item.condition.temp + 'F';
-	} else {
-		temperature = _toCelcius(result.query.results.channel.item.condition.temp) + 'C';
-	}
-
-	let city = cli.input[0] ? cli.input[0] : 'Dhaka';
-	let country = cli.input[1] ? cli.input[1] : 'Bangladesh';
-
-	console.log(chalk.red(city + ', ' + country));
-	console.log(chalk.cyan('Condition: ' + chalk.yellow(condition)));
-	console.log(chalk.cyan('Temperature: ' + chalk.yellow(temperature)));
-	process.exit();
-});
